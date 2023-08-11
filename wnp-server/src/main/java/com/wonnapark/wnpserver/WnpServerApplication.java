6bf4462e2dfcfce32000905b27947d60bd15f245
weonest <geonhee33@naver.com>
2023-08-11 15:42:11 +0900

    Initial Project

---
+++ wnp-server/src/main/java/com/wonnapark/wnpserver/WnpServerApplication.java
@@ -0,0 +1,13 @@
+package com.wonnapark.wnpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WnpServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WnpServerApplication.class, args);
+    }
+
+}
---
+++ wnp-server/src/test/java/com/wonnapark/wnpserver/WnpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wonnapark.wnpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WnpServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
