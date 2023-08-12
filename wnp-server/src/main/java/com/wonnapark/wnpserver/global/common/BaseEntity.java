6dd998955f84927b4d7e82dc31a10d1769ec66e4
weonest <geonhee33@naver.com>
2023-08-12 15:46:58 +0900

    feat: BaseEntity 설정 및 추가

---
+++ wnp-server/src/main/java/com/wonnapark/wnpserver/global/common/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.wonnapark.wnpserver.global.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public abstract class BaseEntity {
+    @Column(name = "create_at", nullable = false, updatable = false)
+    @CreatedDate
+    LocalDateTime createAt;
+
+    @Column(name = "update_at", nullable = false)
+    @LastModifiedDate
+    LocalDateTime updateAt;
+}
