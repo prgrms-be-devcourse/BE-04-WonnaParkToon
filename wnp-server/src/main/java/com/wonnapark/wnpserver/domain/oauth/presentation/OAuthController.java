a5b998e6fbf7f3df8a4ff3daddf650ff16c25321
weonest <geonhee33@naver.com>
2023-08-17 00:01:12 +0900

    refactor: @GetMapping으로 변경

--- wnp-server/src/main/java/com/wonnapark/wnpserver/domain/oauth/presentation/OAuthController.java
+++ wnp-server/src/main/java/com/wonnapark/wnpserver/domain/oauth/presentation/OAuthController.java
@@ -5,10 +5,7 @@ import com.wonnapark.wnpserver.domain.oauth.dto.KakaoLoginRequest;
 import com.wonnapark.wnpserver.domain.oauth.token.AuthToken;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,8 +14,8 @@ public class OAuthController {
 
     private final OAuthLoginService oauthLoginService;
 
-    @PostMapping("/kakao")
-    public ResponseEntity<AuthToken> loginKakao(@RequestBody KakaoLoginRequest request) {
+    @GetMapping("/kakao")
+    public ResponseEntity<AuthToken> loginkakao(@ModelAttribute KakaoLoginRequest request) {
         return ResponseEntity.ok(oauthLoginService.login(request));
     }
 
