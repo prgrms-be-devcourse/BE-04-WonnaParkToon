80f3c12bbd4d0eedc7866afad3f5867c30a314fb
sangminnim <sangmins930@gmail.com>
2023-08-17 00:06:18 +0900

    refactor: LAZY 명시

--- wnp-server/src/main/java/com/wonnapark/wnpserver/domain/episode/EpisodeUrl.java
+++ wnp-server/src/main/java/com/wonnapark/wnpserver/domain/episode/EpisodeUrl.java
@@ -2,6 +2,7 @@ package com.wonnapark.wnpserver.domain.episode;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -24,7 +25,7 @@ public class EpisodeUrl {
     @Column(name = "id", nullable = false)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "episode_id", nullable = false)
     private Episode episode;
 
