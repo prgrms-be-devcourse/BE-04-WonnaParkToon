0945e1c50a88ca9f2e1ae649d295e2b9c8294ea9
weonest <geonhee33@naver.com>
2023-08-17 20:50:21 +0900

    feat: issuer, issuedAt 추가

--- wnp-server/src/main/java/com/wonnapark/wnpserver/domain/oauth/token/JwtTokenGenerator.java
+++ wnp-server/src/main/java/com/wonnapark/wnpserver/domain/oauth/token/JwtTokenGenerator.java
@@ -10,11 +10,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Component
 public class JwtTokenGenerator {
 
+    private static final String ISSUER = "wonnapark";
     private final Key key;
 
     public JwtTokenGenerator(@Value("${jwt.secret-key}") String secretKey) {
@@ -25,6 +28,8 @@ public class JwtTokenGenerator {
     public String generate(String subject, Date expiredAt) {
         return Jwts.builder()
                 .setSubject(subject)
+                .setIssuer(ISSUER)
+                .setIssuedAt(Timestamp.valueOf(LocalDateTime.now()))
                 .setExpiration(expiredAt)
                 .signWith(key, SignatureAlgorithm.HS512)
                 .compact();
@@ -32,6 +37,7 @@ public class JwtTokenGenerator {
 
     public String extractSubject(String accessToken) {
         Claims claims = parseClaims(accessToken);
+        System.out.println("claims.getExpiration() = " + claims.getExpiration());
         return claims.getSubject();
     }
 
