94b0b86588c7d2ea9d19a19d6918ab1d16f4ea18
weonest <geonhee33@naver.com>
2023-08-19 01:39:38 +0900

    refactor: AuthTokenService 로직 수정

--- wnp-server/src/main/java/com/wonnapark/wnpserver/domain/auth/RefreshToken.java
+++ wnp-server/src/main/java/com/wonnapark/wnpserver/domain/auth/RefreshToken.java
@@ -1,18 +1,20 @@
 package com.wonnapark.wnpserver.domain.auth;
 
-import jakarta.persistence.Id;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
 
 @RedisHash(value = "refreshToken", timeToLive = 604800)
 @Getter
 public class RefreshToken {
 
     @Id
-    private String refreshToken;
-
     private Long userId;
+
+    @Indexed
+    private String refreshToken;
 
     @Builder
     private RefreshToken(String refreshToken, Long userId) {
