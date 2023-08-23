aa175e1d1d02ab81ed78ffb3c2ce240e5fec1908
sangminnim <sangmins930@gmail.com>
2023-08-24 03:30:41 +0900

    feat: webtoonId에 따른 Episode Pagenation 조회 구현

--- wnp-server/src/main/java/com/wonnapark/wnpserver/domain/episode/infrastructure/EpisodeRepository.java
+++ wnp-server/src/main/java/com/wonnapark/wnpserver/domain/episode/infrastructure/EpisodeRepository.java
@@ -1,7 +1,12 @@
 package com.wonnapark.wnpserver.domain.episode.infrastructure;
 
 import com.wonnapark.wnpserver.domain.episode.Episode;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface EpisodeRepository extends JpaRepository<Episode, Long> {
+    @Query("SELECT e FROM Episode e WHERE e.webtoon.id = :webtoonId")
+    Page<Episode> findAllById(Long webtoonId, Pageable pageable);
 }
