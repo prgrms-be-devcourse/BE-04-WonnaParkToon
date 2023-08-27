2c00355573f38af52194010af9e86a723ba245e9
weonest <geonhee33@naver.com>
2023-08-27 21:00:39 +0900

    feat: 토큰 관련 에러코드 추가

--- wnp-server/src/main/java/com/wonnapark/wnpserver/global/response/ErrorCode.java
+++ wnp-server/src/main/java/com/wonnapark/wnpserver/global/response/ErrorCode.java
@@ -10,8 +10,10 @@ public enum ErrorCode {
     INTERNAL_SERVER_ERROR(500, "C999", "서버 내부 에러입니다."),
 
     // Token
-    NOT_VALID_TOKEN(400, "T001", "유효하지 않은 토큰입니다."),
-    TOKEN_NOT_FOUND(404, "T001", "토큰이 존재하지 않습니다.");
+    EXPIRED_TOKEN(400, "T001", "만료된 토큰입니다."),
+    UNSUPPORTED_TOKEN(400, "T002", "잘못된 형식의 토큰입니다."),
+    WRONG_SIGNATURE_TOKEN(400, "T003", "변조된 토큰입니다."),
+    TOKEN_NOT_FOUND(404, "T001", "토큰을 찾을 수 없습니다.");
 
     private final int value;
     private final String code;
