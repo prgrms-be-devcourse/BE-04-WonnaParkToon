016e43c12dbfa65b7e13c563e352064bbb444cb7
sangminnim <sangmins930@gmail.com>
2023-08-29 05:54:21 +0900

    feat: create, update, delete 구현

---
+++ wnp-server/src/main/java/com/wonnapark/wnpserver/domain/episode/application/EpisodeManageService.java
@@ -0,0 +1,93 @@
+package com.wonnapark.wnpserver.domain.episode.application;
+
+import com.wonnapark.wnpserver.domain.episode.Episode;
+import com.wonnapark.wnpserver.domain.episode.dto.request.EpisodeArtistCommentUpdateRequest;
+import com.wonnapark.wnpserver.domain.episode.dto.request.EpisodeCreationRequest;
+import com.wonnapark.wnpserver.domain.episode.dto.request.EpisodeReleaseDateTimeUpdateRequest;
+import com.wonnapark.wnpserver.domain.episode.dto.request.EpisodeThumbnailUpdateRequest;
+import com.wonnapark.wnpserver.domain.episode.dto.request.EpisodeTitleUpdateRequest;
+import com.wonnapark.wnpserver.domain.episode.dto.request.EpisodeUrlsUpdateRequest;
+import com.wonnapark.wnpserver.domain.episode.infrastructure.EpisodeRepository;
+import com.wonnapark.wnpserver.domain.webtoon.Webtoon;
+import com.wonnapark.wnpserver.domain.webtoon.infrastructure.WebtoonRepository;
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static com.wonnapark.wnpserver.domain.episode.application.EpisodeErrorMessage.DUPLICATED_EPISODE;
+import static com.wonnapark.wnpserver.domain.episode.application.EpisodeErrorMessage.EPISODE_NOT_FOUND;
+import static com.wonnapark.wnpserver.domain.episode.application.EpisodeErrorMessage.WEBTOON_NOT_FOUND;
+
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class EpisodeManageService implements EpisodeManage {
+
+    private final EpisodeRepository episodeRepository;
+    private final WebtoonRepository webtoonRepository;
+
+    @Override
+    @Transactional
+    public Long createEpisode(Long webtoonId, EpisodeCreationRequest request) {
+        if (episodeRepository.existsByTitle(request.title())) {
+            throw new EntityExistsException(String.format(DUPLICATED_EPISODE, request.title()));
+        }
+        Webtoon webtoon = webtoonRepository.findById(webtoonId)
+                .orElseThrow(() -> new EntityNotFoundException(String.format(WEBTOON_NOT_FOUND, webtoonId)));
+        Episode newEpisode = request.toEntity();
+        newEpisode.setWebtoon(webtoon);
+        return episodeRepository.save(newEpisode).getId();
+    }
+
+    @Override
+    @Transactional
+    public void updateEpisodeTitle(Long episodeId, EpisodeTitleUpdateRequest request) {
+        Episode episode = episodeRepository.findById(episodeId)
+                .orElseThrow(() -> new EntityNotFoundException(String.format(EPISODE_NOT_FOUND, episodeId)));
+        episode.changeTitle(request.title());
+    }
+
+    @Override
+    @Transactional
+    public void updateEpisodeArtistComment(Long episodeId, EpisodeArtistCommentUpdateRequest request) {
+        Episode episode = episodeRepository.findById(episodeId)
+                .orElseThrow(() -> new EntityNotFoundException(String.format(EPISODE_NOT_FOUND, episodeId)));
+        episode.changeArtistComment(request.artistComment());
+    }
+
+    @Override
+    @Transactional
+    public void updateEpisodeThumbnail(Long episodeId, EpisodeThumbnailUpdateRequest request) {
+        Episode episode = episodeRepository.findById(episodeId)
+                .orElseThrow(() -> new EntityNotFoundException(String.format(EPISODE_NOT_FOUND, episodeId)));
+        episode.changeThumbnail(request.thumbnail());
+    }
+
+    @Override
+    @Transactional
+    public void updateEpisodeReleaseDateTime(Long episodeId, EpisodeReleaseDateTimeUpdateRequest request) {
+        Episode episode = episodeRepository.findById(episodeId)
+                .orElseThrow(() -> new EntityNotFoundException(String.format(EPISODE_NOT_FOUND, episodeId)));
+        episode.changeReleaseDateTime(request.releaseDateTime());
+    }
+
+    @Override
+    @Transactional
+    public void updateEpisodeUrls(Long episodeId, EpisodeUrlsUpdateRequest request) {
+        Episode episode = episodeRepository.findById(episodeId)
+                .orElseThrow(() -> new EntityNotFoundException(String.format(EPISODE_NOT_FOUND, episodeId)));
+        episode.changeEpisodeUrls(request.toEntityList());
+    }
+
+    @Override
+    @Transactional
+    public void deleteEpisode(Long episodeId) {
+        Episode episode = episodeRepository.findById(episodeId)
+                .orElseThrow(() -> new EntityNotFoundException(String.format(EPISODE_NOT_FOUND, episodeId)));
+        episode.delete();
+    }
+
+}
