11af48e1dd373d2158b444f45efb83daf89c72e6
sangminnim <sangmins930@gmail.com>
2023-08-29 04:45:37 +0900

    feat: 웹툰ID로 에피소드 조회하는 쿼리메서드 생성

--- wnp-server/src/main/java/com/wonnapark/wnpserver/domain/episode/infrastructure/EpisodeRepository.java
+++ wnp-server/src/main/java/com/wonnapark/wnpserver/domain/episode/infrastructure/EpisodeRepository.java
@@ -4,9 +4,9 @@ import com.wonnapark.wnpserver.domain.episode.Episode;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface EpisodeRepository extends JpaRepository<Episode, Long> {
-    @Query("SELECT e FROM Episode e WHERE e.webtoon.id = :webtoonId")
-    Page<Episode> findAllById(Long webtoonId, Pageable pageable);
+
+    Page<Episode> findAllByWebtoonId(Long webtoonId, Pageable pageable);
+
 }
