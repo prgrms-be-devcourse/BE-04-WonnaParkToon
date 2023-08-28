4511879918a4bb250dca67aaa18ba43182337a16
sangminnim <sangmins930@gmail.com>
2023-08-29 05:35:03 +0900

    feat: existsByTitle

--- wnp-server/src/main/java/com/wonnapark/wnpserver/domain/episode/infrastructure/EpisodeRepository.java
+++ wnp-server/src/main/java/com/wonnapark/wnpserver/domain/episode/infrastructure/EpisodeRepository.java
@@ -9,4 +9,6 @@ public interface EpisodeRepository extends JpaRepository<Episode, Long> {
 
     Page<Episode> findAllByWebtoonId(Long webtoonId, Pageable pageable);
 
+    boolean existsByTitle(String title);
+
 }
