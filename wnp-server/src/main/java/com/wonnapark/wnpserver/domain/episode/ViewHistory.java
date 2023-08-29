d648bcd2994a9d17a6de9455853bab9dda853999
sangminnim <sangmins930@gmail.com>
2023-08-30 02:15:37 +0900

    feat: ViewHistory 의 id 값을 episode id 와 user id의 복합키로 변경

--- wnp-server/src/main/java/com/wonnapark/wnpserver/domain/episode/ViewHistory.java
+++ wnp-server/src/main/java/com/wonnapark/wnpserver/domain/episode/ViewHistory.java
@@ -1,16 +1,7 @@
 package com.wonnapark.wnpserver.domain.episode;
 
-import com.wonnapark.wnpserver.domain.user.User;
-import com.wonnapark.wnpserver.domain.webtoon.Webtoon;
-import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Index;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -18,34 +9,20 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "view_historys", indexes = @Index(name = "index_user_webtoon", columnList = "user_id, webtoon_id"))
-// TODO: 복합 키??, index ??, index 없이 그냥??
+@Table(name = "view_historys")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ViewHistory {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
-    private Long id;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "episode_id", nullable = false)
-    private Episode episode;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "webtoon_id", nullable = false)
-    private Webtoon webtoon;
+    @EmbeddedId
+    ViewHistoryId id;
 
     @Builder
-    private ViewHistory(Episode episode, User user, Webtoon webtoon) {
-        this.episode = episode;
-        this.user = user;
-        this.webtoon = webtoon;
+    private ViewHistory(Long episodeId, Long userId) {
+        id = ViewHistoryId.builder()
+                .episodeId(episodeId)
+                .userId(userId)
+                .build();
     }
 
 }
