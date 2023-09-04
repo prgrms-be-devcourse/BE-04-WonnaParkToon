d613eb4b4d01354e41330852f415f836ae9bbf20
weonest <geonhee33@naver.com>
2023-09-04 22:18:06 +0900

    refactor: AuthenticationContextHolder 로직 좀 더 간결하게 수정

--- wnp-server/src/main/java/com/wonnapark/wnpserver/global/auth/AuthenticationContextHolder.java
+++ wnp-server/src/main/java/com/wonnapark/wnpserver/global/auth/AuthenticationContextHolder.java
@@ -1,22 +1,16 @@
 package com.wonnapark.wnpserver.global.auth;
 
-import org.springframework.util.ObjectUtils;
-
 public class AuthenticationContextHolder {
 
-    private static final ThreadLocal<Authentication> authenticationHolder = new ThreadLocal<>();
+    private static final ThreadLocal<Authentication> authenticationHolder =
+            ThreadLocal.withInitial(Authentication::initAuthentication);
 
     public static void clearContext() {
         authenticationHolder.remove();
     }
 
     public static Authentication getAuthentication() {
-        Authentication authentication = authenticationHolder.get();
-        if (ObjectUtils.isEmpty(authentication)) {
-            authentication = Authentication.initAuthentication();
-            authenticationHolder.set(authentication);
-        }
-        return authentication;
+        return authenticationHolder.get();
     }
 
     public static void setAuthenticationHolder(Authentication authentication) {
