34b114b6c3197de792f945f50e8a1538adf1f15a
sangminnim <sangmins930@gmail.com>
2023-09-07 02:44:34 +0900

    refactor: Pageable을 DTO로 대체

--- wnp-server/src/main/java/com/wonnapark/wnpserver/domain/episode/presentation/CommonEpisodeController.java
+++ wnp-server/src/main/java/com/wonnapark/wnpserver/domain/episode/presentation/CommonEpisodeController.java
@@ -1,14 +1,12 @@
 package com.wonnapark.wnpserver.domain.episode.presentation;
 
 import com.wonnapark.wnpserver.domain.episode.application.EpisodeFindUseCase;
+import com.wonnapark.wnpserver.domain.episode.dto.request.WebtoonListPageRequest;
 import com.wonnapark.wnpserver.domain.episode.dto.response.EpisodeDetailFormResponse;
 import com.wonnapark.wnpserver.domain.episode.dto.response.EpisodeListFormResponse;
 import com.wonnapark.wnpserver.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,9 +34,9 @@ public class CommonEpisodeController {
     @ResponseStatus(OK)
     ApiResponse<Page<EpisodeListFormResponse>> findEpisodeListForm(
             @RequestParam Long webtoonId,
-            @PageableDefault(size = 20, sort = "createAt", direction = Sort.Direction.DESC) Pageable pageable
+            WebtoonListPageRequest webtoonListPageRequest
     ) {
-        Page<EpisodeListFormResponse> episodeListForms = episodeFindUseCase.findEpisodeListForm(webtoonId, pageable);
+        Page<EpisodeListFormResponse> episodeListForms = episodeFindUseCase.findEpisodeListForm(webtoonId, webtoonListPageRequest.toPageable());
         return ApiResponse.from(episodeListForms);
     }
 
--- wnp-server/src/main/java/com/wonnapark/wnpserver/domain/episode/presentation/UserEpisodeController.java
+++ wnp-server/src/main/java/com/wonnapark/wnpserver/domain/episode/presentation/UserEpisodeController.java
@@ -1,14 +1,12 @@
 package com.wonnapark.wnpserver.domain.episode.presentation;
 
 import com.wonnapark.wnpserver.domain.episode.application.EpisodeFindUseCase;
+import com.wonnapark.wnpserver.domain.episode.dto.request.WebtoonListPageRequest;
 import com.wonnapark.wnpserver.domain.episode.dto.response.EpisodeDetailFormResponse;
 import com.wonnapark.wnpserver.domain.episode.dto.response.EpisodeListFormResponse;
 import com.wonnapark.wnpserver.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,10 +35,10 @@ public class UserEpisodeController {
     @ResponseStatus(OK)
     ApiResponse<Page<EpisodeListFormResponse>> findEpisodeListForm(
             @RequestParam Long webtoonId,
-            @PageableDefault(size = 20, sort = "createAt", direction = Sort.Direction.DESC) Pageable pageable
+            WebtoonListPageRequest webtoonListPageRequest
     ) {
         Long userId = 1L; // TODO: parameter에 UserInfo 추가하고 삭제
-        Page<EpisodeListFormResponse> episodeListForms = episodeFindUseCase.findEpisodeListForm(userId, webtoonId, pageable);
+        Page<EpisodeListFormResponse> episodeListForms = episodeFindUseCase.findEpisodeListForm(userId, webtoonId, webtoonListPageRequest.toPageable());
         return ApiResponse.from(episodeListForms);
     }
 
