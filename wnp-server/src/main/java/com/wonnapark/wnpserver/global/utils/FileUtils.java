b763e1e28dff21412e091828fb67c07b3dcf1c13
sangminnim <sangmins930@gmail.com>
2023-09-09 17:36:55 +0900

    feat #42 : FileIOException 생성 및 예외 처리

--- wnp-server/src/main/java/com/wonnapark/wnpserver/global/utils/FileUtils.java
+++ wnp-server/src/main/java/com/wonnapark/wnpserver/global/utils/FileUtils.java
@@ -9,11 +9,24 @@ import java.io.IOException;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class FileUtils {
+    private static final String MULTIPART_FILE_TRANSFER_ERROR = "MULTIPART 파일을 FILE로 변환 중 오류가 발생했습니다.";
 
-    public static File convertMultipartFileToFile(MultipartFile multipartFile) throws IOException {
-        File file = File.createTempFile("temp", multipartFile.getOriginalFilename());
-        multipartFile.transferTo(file);
-        return file;
+    public static class FileIOException extends RuntimeException {
+
+        public FileIOException(String message, Throwable e) {
+            super(message, e);
+        }
+
+    }
+
+    public static File convertMultipartFileToFile(MultipartFile multipartFile) {
+        try {
+            File file = File.createTempFile("temp", multipartFile.getOriginalFilename());
+            multipartFile.transferTo(file);
+            return file;
+        } catch (IOException e) {
+            throw new FileIOException(MULTIPART_FILE_TRANSFER_ERROR, e);
+        }
     }
 
 }
