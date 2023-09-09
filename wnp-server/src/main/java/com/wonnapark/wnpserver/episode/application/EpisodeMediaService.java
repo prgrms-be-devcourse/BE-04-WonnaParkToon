1f71ac1f519345c285002e50d2aa5f91409cd6f7
sangminnim <sangmins930@gmail.com>
2023-09-09 17:38:17 +0900

    feat #42 : 이미지 저장 service 생성

---
+++ wnp-server/src/main/java/com/wonnapark/wnpserver/episode/application/EpisodeMediaService.java
@@ -0,0 +1,50 @@
+package com.wonnapark.wnpserver.episode.application;
+
+import com.wonnapark.wnpserver.episode.dto.response.EpisodeMediaUploadResponse;
+import com.wonnapark.wnpserver.media.S3MediaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class EpisodeMediaService {
+
+    private static final String THUMBNAIL_KEY_PATTERN = "webtoon/%s/thumbnail_202x120_%s";
+    private static final String EPISODE_URL_KEY_PATTERN = "webtoon/%s/%s_%s_IMAG01_%s";
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+
+    private final S3MediaService s3MediaService;
+
+    public EpisodeMediaUploadResponse uploadEpisodeMedia(String webtoonId, MultipartFile thumbnail, List<MultipartFile> episodeImages) {
+        return new EpisodeMediaUploadResponse(
+                uploadThumbnail(webtoonId, thumbnail),
+                uploadEpisodeImages(webtoonId, episodeImages)
+        );
+    }
+
+    private String uploadThumbnail(String webtoonId, MultipartFile thumbnail) {
+        String key = String.format(THUMBNAIL_KEY_PATTERN, webtoonId, UUID.randomUUID());
+        return s3MediaService.upload(key, thumbnail);
+    }
+
+    private List<String> uploadEpisodeImages(String webtoonId, List<MultipartFile> episodeImages) {
+        String uploadedDateTime = LocalDateTime.now().format(formatter);
+        UUID uuid = UUID.randomUUID();
+
+        List<String> urls = new ArrayList<>();
+        for (int imageOrder = 1; imageOrder <= episodeImages.size(); imageOrder++) {
+            String key = String.format(EPISODE_URL_KEY_PATTERN, webtoonId, uploadedDateTime, uuid, imageOrder);
+            int imagesIndex = imageOrder - 1;
+            urls.add(s3MediaService.upload(key, episodeImages.get(imagesIndex)));
+        }
+        return urls;
+    }
+
+}
