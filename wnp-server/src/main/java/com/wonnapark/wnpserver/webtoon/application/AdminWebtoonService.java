60948eda9d23d20369bb247d8c9ba02e50ce3432
na-yk <ninghao17@naver.com>
2023-09-10 14:53:41 +0900

    feat: 웹툰 생성 API 요청에서 thumbnail 파일 받도록 변경
    웹툰 생성 API thumbnail을 URL 문자열로 받아 저장하던 방식에서 file로 받아 S3로 업로드하고 해당 URL을 저장하는 방식으로 변경함

--- wnp-server/src/main/java/com/wonnapark/wnpserver/webtoon/application/AdminWebtoonService.java
+++ wnp-server/src/main/java/com/wonnapark/wnpserver/webtoon/application/AdminWebtoonService.java
@@ -1,5 +1,6 @@
 package com.wonnapark.wnpserver.webtoon.application;
 
+import com.wonnapark.wnpserver.media.S3ManageService;
 import com.wonnapark.wnpserver.webtoon.Webtoon;
 import com.wonnapark.wnpserver.webtoon.dto.request.WebtoonCreateRequest;
 import com.wonnapark.wnpserver.webtoon.dto.request.WebtoonUpdateRequest;
@@ -10,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 
 @Service
@@ -18,10 +20,16 @@ import java.time.LocalDateTime;
 public class AdminWebtoonService {
 
     private final WebtoonRepository webtoonRepository;
+    private final S3ManageService s3ManageService;
+    private final String THUMBNAIL = "thumbnail";
 
     @Transactional
-    public WebtoonDetailResponse createWebtoon(WebtoonCreateRequest request) {
-        Webtoon webtoon = WebtoonCreateRequest.toEntity(request);
+    public WebtoonDetailResponse createWebtoon(WebtoonCreateRequest request) throws IOException {
+        String key = createFileName(request.title(), request.thumbnail().getOriginalFilename());
+        s3ManageService.upload(key, request.thumbnail());
+        String thumbnailUrl = s3ManageService.findObjectUrlByName(key);
+        Webtoon webtoon = WebtoonCreateRequest.toEntity(request, thumbnailUrl);
+
         return WebtoonDetailResponse.from(webtoonRepository.save(webtoon));
     }
 
@@ -49,6 +57,27 @@ public class AdminWebtoonService {
         webtoon.delete();
 
         return webtoon.getIsDeleted();
+    }
+
+    /**
+     * [웹툰 작품명/thumbnail.확장자] 형식으로 파일 이름을 생성
+     *
+     * @param folderName       파일을 저장할 폴더 이름
+     * @param originalFileName 파일 원본 이름
+     * @return 저장 위치를 포함한 이름
+     */
+    private String createFileName(String folderName, String originalFileName) {
+        String fileExtension = getFileExtension(originalFileName);
+        String fileName = THUMBNAIL.concat(fileExtension);
+        return folderName + "/" + fileName;
+    }
+
+    private String getFileExtension(String fileName) {
+        try {
+            return fileName.substring(fileName.lastIndexOf("."));
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new IllegalArgumentException(String.format("잘못된 파일 형식입니다: %s", fileName));
+        }
     }
 
 }
