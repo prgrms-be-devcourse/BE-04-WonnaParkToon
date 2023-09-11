21a2c3a47840064c678b8e2e6dfdd37d2b0a550c
sangminnim <sangmins930@gmail.com>
2023-09-11 16:34:09 +0900

    test: main 코드 변경 반영 및 코드 최적화

--- wnp-server/src/test/java/com/wonnapark/wnpserver/domain/episode/application/EpisodeMediaServiceTest.java
+++ wnp-server/src/test/java/com/wonnapark/wnpserver/domain/episode/application/EpisodeMediaServiceTest.java
@@ -8,19 +8,20 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
 
 @ExtendWith(MockitoExtension.class)
 class EpisodeMediaServiceTest {
@@ -34,16 +35,16 @@ class EpisodeMediaServiceTest {
     void uploadEpisodeMedia() {
         // given
         String webtoonId = "1";
-        MultipartFile thumbnail = new MockMultipartFile("thumbnail", "thumbnail".getBytes());
-        List<MultipartFile> episodeImages = new ArrayList<>(Arrays.asList(
-                new MockMultipartFile("image1", "image1".getBytes()),
-                new MockMultipartFile("image2", "image2".getBytes()),
-                new MockMultipartFile("image3", "image3".getBytes()),
-                new MockMultipartFile("image4", "image4".getBytes()),
-                new MockMultipartFile("image5", "image5".getBytes())
+        File thumbnail = new File("path/thumbnail");
+        List<File> episodeImages = new ArrayList<>(Arrays.asList(
+                new File("path/image1"),
+                new File("path/image2"),
+                new File("path/image3"),
+                new File("path/image4"),
+                new File("path/image5")
         ));
         String expectedThumbnailUrl = "https://sample.thumbnail.jpeg";
-        List<String> expectedImageUrls = IntStream.range(1, 6)
+        List<String> expectedImageUrls = IntStream.range(1, episodeImages.size() + 1)
                 .mapToObj(operand -> String.format("https://sample.image_%d.jpeg", operand))
                 .toList();
 
@@ -58,10 +59,11 @@ class EpisodeMediaServiceTest {
         // then
         assertThat(episodeMediaUploadResponse.thumbnailUrl()).isEqualTo(expectedThumbnailUrl);
         assertThat(episodeMediaUploadResponse.urls()).isEqualTo(expectedImageUrls);
-        then(s3MediaService).should().upload(anyString(), eq(thumbnail));
-        IntStream.range(0, 5).forEach(i ->
-                then(s3MediaService).should().upload(anyString(), eq(episodeImages.get(i)))
-        );
+
+        int thumbnailUploadTime = 1;
+        then(s3MediaService).should(
+                times(thumbnailUploadTime + episodeImages.size())
+        ).upload(anyString(), any(File.class));
     }
 
 }
