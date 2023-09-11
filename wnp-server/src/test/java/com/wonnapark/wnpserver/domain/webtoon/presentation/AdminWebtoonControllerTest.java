bb8edb72e5a3df9c24bb4042327b50b3127696fb
na-yk <ninghao17@naver.com>
2023-09-11 22:49:14 +0900

    test: AdminWebtoonController createWebtoon 테스트 수정
    jwtAuthenticationInterceptor 설정 추가 및 responseField 추가

--- wnp-server/src/test/java/com/wonnapark/wnpserver/domain/webtoon/presentation/AdminWebtoonControllerTest.java
+++ wnp-server/src/test/java/com/wonnapark/wnpserver/domain/webtoon/presentation/AdminWebtoonControllerTest.java
@@ -1,8 +1,11 @@
 package com.wonnapark.wnpserver.domain.webtoon.presentation;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.wonnapark.wnpserver.webtoon.Webtoon;
+import com.wonnapark.wnpserver.auth.application.AuthenticationResolver;
 import com.wonnapark.wnpserver.domain.webtoon.WebtoonFixtures;
+import com.wonnapark.wnpserver.global.auth.AuthorizedArgumentResolver;
+import com.wonnapark.wnpserver.global.auth.JwtAuthenticationInterceptor;
+import com.wonnapark.wnpserver.webtoon.Webtoon;
 import com.wonnapark.wnpserver.webtoon.application.AdminWebtoonService;
 import com.wonnapark.wnpserver.webtoon.dto.request.WebtoonCreateRequest;
 import com.wonnapark.wnpserver.webtoon.dto.request.WebtoonUpdateRequest;
@@ -21,7 +24,10 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
 import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.resourceDetails;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
@@ -29,8 +35,10 @@ import static org.springframework.restdocs.operation.preprocess.Preprocessors.pr
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(AdminWebtoonController.class)
@@ -40,26 +48,41 @@ class AdminWebtoonControllerTest {
 
     @Autowired
     ObjectMapper objectMapper;
+
     @Autowired
     private MockMvc mockMvc;
+
     @MockBean
     private AdminWebtoonService adminWebtoonService;
+
+    @MockBean
+    AuthenticationResolver authenticationResolver;
+
+    @MockBean
+    AuthorizedArgumentResolver authorizedArgumentResolver;
+
+    @MockBean
+    JwtAuthenticationInterceptor jwtAuthenticationInterceptor;
 
     @Test
     @DisplayName("새로운 웹툰을 생성하고 웹툰 상세 정보를 반환할 수 있다.")
     void createWebtoon() throws Exception {
         // given
-        WebtoonCreateRequest webtoonCreateRequest = WebtoonFixtures.createWebtoonCreateRequest();
-        Webtoon webtoon = WebtoonCreateRequest.toEntity(webtoonCreateRequest);
-        given(adminWebtoonService.createWebtoon(webtoonCreateRequest)).willReturn(WebtoonDetailResponse.from(webtoon));
+        given(jwtAuthenticationInterceptor.preHandle(any(), any(), any())).willReturn(true);
+
+        WebtoonCreateRequest request = WebtoonFixtures.createWebtoonCreateRequest();
+        Webtoon webtoon = WebtoonFixtures.createWebtoon(request);
+        WebtoonDetailResponse response = WebtoonDetailResponse.from(webtoon);
+        given(adminWebtoonService.createWebtoon(request)).willReturn(response);
 
         // when, then
         mockMvc.perform(post("/api/v1/admin/webtoons")
                         .contentType("application/json")
-                        .content(objectMapper.writeValueAsString(webtoonCreateRequest)))
+                        .content(objectMapper.writeValueAsString(request)))
                 .andDo(print())
                 .andExpect(status().isCreated())
                 .andExpect(header().string("Location", String.format("/api/v1/webtoons/%d", webtoon.getId())))
+                .andExpect(jsonPath("data.id").value(webtoon.getId()))
                 .andDo(document("admin-webtoon-v1-post-createWebtoon",
                         resourceDetails().tag("웹툰-관리자").description("웹툰 생성"),
                         preprocessRequest(prettyPrint()),
@@ -69,9 +92,19 @@ class AdminWebtoonControllerTest {
                                 fieldWithPath("artist").type(JsonFieldType.STRING).description("웹툰 작가"),
                                 fieldWithPath("detail").type(JsonFieldType.STRING).description("웹툰 설명"),
                                 fieldWithPath("genre").type(JsonFieldType.STRING).description("웹툰 장르"),
-                                fieldWithPath("thumbnail").type(JsonFieldType.STRING).description("웹툰 썸네일"),
                                 fieldWithPath("ageRating").type(JsonFieldType.STRING).description("웹툰 연령 등급"),
                                 fieldWithPath("publishDays").type(JsonFieldType.ARRAY).description("웹툰 연재 요일")
+                        ),
+                        responseHeaders(headerWithName("Location").description("새로 생성된 웹툰 URI")),
+                        responseFields(
+                                fieldWithPath("data.id").type(JsonFieldType.NUMBER).description("웹툰 ID"),
+                                fieldWithPath("data.title").type(JsonFieldType.STRING).description("웹툰 제목"),
+                                fieldWithPath("data.artist").type(JsonFieldType.STRING).description("웹툰 작가"),
+                                fieldWithPath("data.detail").type(JsonFieldType.STRING).description("웹툰 설명"),
+                                fieldWithPath("data.genre").type(JsonFieldType.STRING).description("웹툰 장르"),
+                                fieldWithPath("data.thumbnail").type(JsonFieldType.STRING).description("웹툰 썸네일"),
+                                fieldWithPath("data.ageRating").type(JsonFieldType.STRING).description("웹툰 연령 등급"),
+                                fieldWithPath("data.publishDays").type(JsonFieldType.ARRAY).description("웹툰 연재 요일")
                         )
                 ));
     }
