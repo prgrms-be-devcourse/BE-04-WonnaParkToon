1a08e6932155de1dac7d6a75ba47550013d2a305
na-yk <ninghao17@naver.com>
2023-09-23 12:15:35 +0900

    feat: CacheConfig 추가

---
+++ wnp-server/src/main/java/com/wonnapark/wnpserver/global/config/CacheConfig.java
@@ -0,0 +1,38 @@
+package com.wonnapark.wnpserver.global.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@EnableCaching
+@Configuration
+public class CacheConfig {
+
+    @Bean
+    public CacheManager cacheManager() {
+        List<CaffeineCache> caches = Arrays.stream(CacheType.values())
+                .map(cacheType ->
+                        new CaffeineCache(
+                                cacheType.getCacheName(),
+                                Caffeine.newBuilder()
+                                        .recordStats()
+                                        .expireAfterWrite(cacheType.getSecsToExpireAfterWrite(), TimeUnit.SECONDS)
+                                        .maximumSize(cacheType.getMaximumSize())
+                                        .build()))
+                .toList();
+
+        SimpleCacheManager cacheManager = new SimpleCacheManager();
+        cacheManager.setCaches(caches);
+
+        return cacheManager;
+    }
+    
+}
